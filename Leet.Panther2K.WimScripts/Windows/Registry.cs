b7cb693cb5cfacd163f2d3e201b4626e7a22e329
Anis Errais <anis1errais@live.com>
2025-07-02 16:41:47 +0200

    Renamed root source folders to their namespaces Added pantherScripts .NET rewrite

---
+++ Leet.Panther2K.WimScripts/Windows/Registry.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows
+{
+    internal static class Registry
+    {
+        [DllImport("advapi32.dll", EntryPoint = "RegLoadAppKey", SetLastError = true)]
+        public static extern int LoadAppKey(
+        string lpFile,
+        out IntPtr phkResult,
+        uint samDesired,
+        uint dwOptions,
+        uint Reserved);
+
+        [DllImport("advapi32.dll", EntryPoint = "RegFlushKey", SetLastError = true)]
+        public static extern int FlushKey(IntPtr hKey);
+
+        [DllImport("advapi32.dll", EntryPoint = "RegCloseKey", SetLastError = true)]
+        public static extern int CloseKey(IntPtr hKey);
+    }
+}
